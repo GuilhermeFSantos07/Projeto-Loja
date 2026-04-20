4708506004db580bd5ceda5bb840a548c8b79e7e
Guilherme Farias dos Santos <guilherme.f.santos01702@gmail.com>
2026-04-20 17:00:50 -0400

    Atualizando rotas para o login

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,76 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import Header from "./app/components/Header";
-import { AppProvider } from "./context/AppContext";
-import { lazy, Suspense } from "react";
+import { AppProvider, useAppContext } from "./context/AppContext";
+import Login from "./app/view/Login";
+import Vendas from "./app/view/Vendas";
+import ListaProdutos from "./app/view/ListaProdutos";
+import CadastroAtualizacao from "./app/view/CadastroAtualizacao";
+import Relatorios from "./app/view/Relatorios";
 
-const Vendas = lazy(() => import('./app/view/Vendas'));
-const CadastroAtualizacao = lazy(() => import('./app/view/CadastroAtualizacao'));
-const ListaProdutos = lazy(() => import('./app/view/ListaProdutos'));
-const Relatorios = lazy(() => import('./app/view/Relatorios'));
+
+const PrivateRoute = ({children, cargosPermitidos}:{children: JSX.Element, cargosPermitidos?: string[]}) => {
+  const {usuarioLogado} = useAppContext();
+
+  if (!usuarioLogado){
+    return <Navigate to ="/login"/>;
+  }
+
+  if (cargosPermitidos && !cargosPermitidos.includes(usuarioLogado.cargo)) {
+    return <Navigate to="/vendas"/>
+  }
+
+  return children;
+}
 
 function App() {
   return (
     <AppProvider>
-      <BrowserRouter>
-        <div className="min-h-screen bg-gray-100">
-          <Header/>
-          <Suspense fallback={<div className="pt-32 text-center text-xl font-bold text-gray-500">Carregando tela...</div>}>
-              <Routes>
-              <Route path="/" element={<Navigate to="/vendas" replace />}/>
-              <Route path="/vendas" element={<Vendas/>}/>
-              <Route path="/cadastro" element={<CadastroAtualizacao/>}/>
-              <Route path="/produtos" element={<ListaProdutos/>}/>
-              <Route path="/relatorios" element={<Relatorios/>} />
-            </Routes>
-          </Suspense>
-        </div>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/" element={<Navigate to="/vendas"/>}/>
+          <Route path="/vendas" element={
+            <PrivateRoute>
+              <>
+                <Header/>
+                <div className="min-h-screen bg-gray-100"><Vendas /></div>
+              </>
+            </PrivateRoute>
+          } />
+          <Route path="/produtos" element={
+            <PrivateRoute>
+              <>
+                <Header/>
+                <div className="min-h-screen bg-gray-100"><ListaProdutos /></div>
+              </>
+            </PrivateRoute>
+          } />
+          <Route path="/cadastro" element={
+            <PrivateRoute cargosPermitidos={['gerente', 'dev']}>
+              <>
+                <Header/>
+                <div className="min-h-screen bg-gray-100"><CadastroAtualizacao /></div>
+              </>
+            </PrivateRoute>
+          } />
+          <Route path="/relatorios" element={
+            <PrivateRoute cargosPermitidos={['gerente', 'dev']}>
+              <>
+                <Header/>
+                <div className="min-h-screen bg-gray-100"><Relatorios /></div>
+              </>
+            </PrivateRoute>
+          }/>
+          <Route path="/usuarios" element={
+            <PrivateRoute>
+              <>
+                <Header/>
+                <div className="min-h-screen bg-gray-100"><GerenciamentoUsuarios /></div>
+              </>
+            </PrivateRoute>
+          }/>
+        </Routes>
+      </Router>
     </AppProvider>
   );
 };
